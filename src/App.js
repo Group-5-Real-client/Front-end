178953ca4d6f3beb2c91cd90e0e29648f6e4cee5
Aya Shahal <ayashahal055@gmail.com>
2023-04-25 09:21:38 +0300

    login form done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from './Pages/AboutUs/index';
 import ContactUs from './Pages/ContactUs/index';
 import Event from './Pages/Event/index';
 import Signup  from './Pages/Signup';
+import Signin from './Pages/Signin';
 function App() {
   return (
     
@@ -15,6 +16,7 @@ function App() {
           <Route path="Event" element={<Event />} />
           <Route path="Contact" element={<ContactUs />} />
           <Route path="Register" element={<Signup />} />
+          <Route path="login" element={<Signin />} />
       </Routes>
       <Footer/>
       </BrowserRouter>
---
+++ src/Pages/Signin/index.js
@@ -0,0 +1,10 @@
+import Login from "../../Component/Login/index.js";
+
+
+function Signin(){
+    return(
+    <Login/>
+    )
+}
+
+export default Signin
