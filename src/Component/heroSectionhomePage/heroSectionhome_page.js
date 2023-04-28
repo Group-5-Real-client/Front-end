63a69f3a50c4262b3ff846b95b7f89f2d4215b6c
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-04-29 00:32:02 +0300

    slider images

--- src/Component/heroSectionhomePage/heroSectionhome_page.js
+++ src/Component/heroSectionhomePage/heroSectionhome_page.js
@@ -3,39 +3,39 @@ import React from "react";
 import Slider from "react-slick";
 import { useState, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
+import CustomSlider from "./custom.slider";
+import images from "./images";
 import "./index.css";
-import img1 from "../../images/pic_1.jpg";
-import img2 from "../../images/pic_1.jpg";
-import img3 from "../../images/pic_1.jpg";
+import "./custom.slider.css"
+// import img1 from "../../images/pic_1.jpg";
+// import img2 from "../../images/pic_1.jpg";
+// import img3 from "../../images/pic_1.jpg";
 
 function HeroSectionHome() {
-
   return (
     <>
       <section className="home_hero_body">
-
         <main className="">
-          <section className="home_hero_section">
+          <div className="App">
+            <CustomSlider>
+              {images.map((image, index) => {
+                return (
+                  <img key={index} src={image.imgURL} alt={image.imgAlt} />
+                );
+              })}
+            </CustomSlider>
+          </div>
 
-            <div className="home_hero_container home_hero_row">
-              <div className="home_hero_wrap_top">
-                <h1 className="home_hero_title">
-                  <span className="home_hero_accent_one">The</span>{" "}
-                  <span className="home_hero_accent_two">Rooted</span>
-                </h1>
-              </div>
-              <div className="home_wrap_bottom">
-                <p className="home_hero_text">welcome to our community</p>
-                <div>
-                  <Link
-                    to="/register"
-                    className="home_hero_link  home_hero_link_button">
-                    Join Today
-                  </Link>
-                </div>
-              </div>
+          <div className="home_wrap_bottom">
+            <p className="home_hero_text">welcome to our community</p>
+            <div>
+              <Link
+                to="/register"
+                className="home_hero_link home_hero_link_button">
+                Join Today
+              </Link>
             </div>
-          </section>
+          </div>
         </main>
       </section>
     </>
@@ -57,7 +57,6 @@ export default HeroSectionHome;
 // function HeroSectionHome() {
 
 //   return (
-
 
 //   return (
 //     <div className="hero-container">
@@ -88,12 +87,8 @@ export default HeroSectionHome;
 //         </main>
 //       </div>
 
-     
 //     </div>
 //   );
 // }
 
 // export default HeroSectionHome;
-
-
-
