9c0e6ceb89cbfe6d99121f260f4dd702bef8a9ae
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-05-03 10:35:36 +0300

    added blog on home page

--- src/Component/sellershomePage/sellershomePage.js
+++ src/Component/sellershomePage/sellershomePage.js
@@ -77,6 +77,47 @@ function sellershomePage() {
             </div>
           </section>
         </div>
+
+
+         {/* Part 3: Blog section */}
+      <div className="aboutUs-section aboutUs-blog">
+        <h2>Our Blog</h2>
+        <p>Blog content goes here</p>
+      </div>
+
+      {/* Part 4: Three cards */}
+  <div className="aboutUs-section aboutUs-card-container1">
+        <div className="aboutUs-card1">
+          <h3>How to Reduce Waste in Your Kitchen</h3>
+          <h4>Tips for a Zero-Waste Kitchen</h4>
+          <p> {showMore1
+              ? "Looking for ways to reduce waste in your kitchen? We've got you covered. From reusable containers and bags to composting and meal planning, there are many simple steps you can take to minimize your environmental footprint. In this blog post, we'll share our top tips for creating a zero-waste kitchen that is both eco-friendly and functional."
+              : "Looking for ways to reduce waste in your kitchen? We've..."}
+            <button onClick={toggleShowMore1} className="btn-readmore">
+              {showMore1 ? "Read less" : "Read more"}
+            </button></p>
+        </div>
+        <div className="aboutUs-card1">
+          <h3>The Benefits of Using Natural Personal Care Products</h3>
+          <h4>Why You Should Switch to Natural Products</h4>
+          <p>{showMore2
+              ? "Did you know that many personal care products contain harmful chemicals that can be harmful to your health and the environment? Fortunately, there is a growing movement towards using natural and organic products that are free from synthetic chemicals and toxins. In this blog post, we'll explore the benefits of using natural personal care products and how they can help you live a healthier, more sustainable lifestyle."
+              : "Did you know that many personal care products contain..."}
+            <button onClick={toggleShowMore2} className="btn-readmore">
+              {showMore2 ? "Read less" : "Read more"}
+            </button></p>
+        </div>
+        <div className="aboutUs-card1">
+          <h3>How to Make a Positive Impact on the Planet</h3>
+          <h4>Simple Steps for a Sustainable Life</h4>
+          <p>{showMore3
+              ? "Living a sustainable lifestyle doesn't have to be difficult or expensive. In fact, there are many simple steps you can take to reduce your environmental impact and create a better future for the planet. From reducing waste and conserving energy to supporting sustainable businesses and investing in renewable resources, there are many ways to make a positive impact on the planet. In this blog post, we'll share our top tips for living a more sustainable life and making a difference in the world."
+              : "Living a sustainable lifestyle doesn't have to be difficult..."}
+            <button onClick={toggleShowMore3} className="btn-readmore">
+              {showMore3 ? "Read less" : "Read more"}
+            </button></p>
+        </div>
+      </div>
       </main>
     </>
   );
