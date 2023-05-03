5b974d426b08bb04348efa0e6ba2d40b873e0952
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-05-03 09:18:21 +0300

    edit on sellerHome page

--- src/Component/sellershomePage/sellershomePage.js
+++ src/Component/sellershomePage/sellershomePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
-
+import axios from "axios";
 import "./index.css";
 
 const arraySeller = [
@@ -36,11 +36,10 @@ const arraySeller = [
     price: "$12.50",
     percent: "97%",
   },
-
-
 ];
 
 function sellershomePage() {
+  
   return (
     <>
       <main className="about_parent-seller">
@@ -79,79 +78,6 @@ function sellershomePage() {
           </section>
         </div>
       </main>
-      {/* <main className="about_parent-seller">
-        <hr className="about_hr-seller" />
-        <div className="about_container-seller">
-          <section className="about_our_blog_container-seller">
-            <div className="about_title_blog-seller">
-              <h2 className="ourBloc_title-seller">Best Sellers</h2>
-            </div>
-
-            <div className="about_mission_container-seller">
-              <div className="mission_box-seller   ">
-                <img
-                  className="about_img-seller  img___one"
-                  src="https://placekitten.com/430/350"
-                  alt="cat"
-                />
-             
-                <div className="product-category-seller">
-                  <span className="product-name-seller">Product Name</span>
-                  <span className="category-name-seller">CATEGORY</span>
-                </div>
-
-                <div className="boxes___container-seller">
-                  <span className="pay-value-seller    value___for_two">$45.95</span>
-                  <span className="percentage-value-seller   value___for_two">-40%</span>
-                </div>
-              </div>
-
-              <section className="mission_box-seller ">
-                <img
-                  className="about_img-seller   img___two"
-                  src="https://placekitten.com/430/350"
-                  alt="cat"
-                />
-               
-
-                <div className="product-category-seller">
-                  <span className="product-name-seller">Product Name</span>
-                  <span className="category-name-seller">CATEGORY</span>
-                </div>
-
-                <div className="boxes___container-seller">
-                  <span className="pay-value-seller   value___for_two">$45.95</span>
-                  <span className="percentage-value-seller  value___for_two">-40%</span>
-                </div>
-              </section>
-
-
-
-
-              <section className="mission_box-seller ">
-                <img
-                  className="about_img-seller   img___three"
-                  src="https://placekitten.com/430/350"
-                  alt="cat"
-                />
-               
-
-                <div className="product-category-seller">
-                  <span className="product-name-seller">Product Name</span>
-                  <span className="category-name-seller">CATEGORY</span>
-                </div>
-
-                <div className="boxes___container-seller">
-                  <span className="pay-value-seller   value___for_two">$45.95</span>
-                  <span className="percentage-value-seller  value___for_two">-40%</span>
-                </div>
-              </section>
-
-              
-            </div>
-          </section>
-        </div>
-      </main> */}
     </>
   );
 }
