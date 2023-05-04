eb9426bc14fa4e98bab3ec28779115c1472ea0b0
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-05-04 20:33:52 +0300

    finished admintable with all functionalities working 100%

--- src/Component/adminTableDash/adminTableDash.jsx
+++ src/Component/adminTableDash/adminTableDash.jsx
@@ -23,50 +23,89 @@ function AdminTable() {
         },
     ]);
 
+    const [newAdminName, setNewAdminName] = useState("");
+    const [newAdminRole, setNewAdminRole] = useState("Admin");
+    const [newAdminEmail, setNewAdminEmail] = useState("");
+    const [newAdminPassword, setNewAdminPassword] = useState("");
+    const [newAdminIsSuper, setNewAdminIsSuper] = useState(false);
+
     const [open, setOpen] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingAdmin, setEditingAdmin] = useState(null);
+    const [filter, setFilter] = useState("");
+
     const showModal = () => {
+        setEditingAdmin(null);
+        setIsEditing(false);
         setOpen(true);
     };
-    const handleOk = (e) => {
-        console.log(e);
-        setOpen(false);
-    };
-    const handleCancel = (e) => {
-        console.log(e);
+
+    const handleOk = () => {
+        if (editingAdmin) {
+            handleSaveAdmin(editingAdmin);
+        } else {
+            handleAddAdmin();
+        }
         setOpen(false);
     };
 
-    const [editingAdmin, setEditingAdmin] = useState(null);
-    const [filter, setFilter] = useState("");
+    const handleCancel = () => {
+        setIsEditing(false);
+        setOpen(false);
+        setNewAdminName("");
+        setNewAdminEmail("");
+        setNewAdminPassword("");
+        setNewAdminIsSuper(false);
+        setNewAdminRole("Admin");
+        setEditingAdmin(null);
+    };
 
     const handleAddAdmin = () => {
         const newAdmin = {
             id: admins.length + 1,
-            name: "New Admin",
-            email: "newadmin@example.com",
-            role: "Viewer",
+            name: newAdminName,
+            email: newAdminEmail,
+            password: newAdminPassword,
+            role: newAdminIsSuper ? "SuperAdmin" : "Admin",
         };
         setAdmins([...admins, newAdmin]);
+        setOpen(false);
+        setNewAdminName("");
+        setNewAdminEmail("");
+        setNewAdminPassword("");
+        setNewAdminIsSuper(false);
+        setNewAdminRole("Admin");
     };
 
     const handleEditAdmin = (admin) => {
         if (admin) {
             setEditingAdmin(admin);
+            setNewAdminName(admin.name);
+            setNewAdminEmail(admin.email);
+            setNewAdminPassword(admin.password);
+            setNewAdminIsSuper(admin.role === "SuperAdmin");
+            setNewAdminRole(admin.role);
+            setIsEditing(true);
+            setOpen(true);
         } else {
-            setEditingAdmin(null);
+            handleCancel();
         }
     };
 
     const handleSaveAdmin = (editedAdmin) => {
+        const updatedAdmin = {
+            ...editedAdmin,
+            role: newAdminIsSuper ? "SuperAdmin" : "Admin",
+        };
         setAdmins(
             admins.map((admin) =>
-                admin.id === editedAdmin.id ? editedAdmin : admin
+                admin.id === editedAdmin.id ? updatedAdmin : admin
             )
         );
         setEditingAdmin(null);
+        setIsEditing(false);
         setFilter("");
     };
-
     const handleDeleteAdmin = (adminId) => {
         setAdmins(admins.filter((admin) => admin.id !== adminId));
     };
@@ -90,26 +129,86 @@ function AdminTable() {
         <>
             <div className="dash-main">
                 <h2>Admins List</h2>
-                <div>
+                <div className="add-button">
                     <Button type="primary" onClick={showModal}>
                         Add Admin
                     </Button>
                     <Modal
-                        title="Basic Modal"
-                        open={open}
+                        title={editingAdmin ? "Edit Admin" : "Add Admin"}
+                        visible={open}
                         onOk={handleOk}
                         onCancel={handleCancel}
-                        okButtonProps={{
-                            disabled: true,
-                        }}
-                        cancelButtonProps={{
-                            disabled: true,
-                        }}
                     >
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
+                        <div>
+                            <label>Username:</label>
+                            <input
+                                type="text"
+                                value={
+                                    editingAdmin
+                                        ? editingAdmin.name
+                                        : newAdminName
+                                }
+                                onChange={(e) =>
+                                    editingAdmin
+                                        ? setEditingAdmin({
+                                              ...editingAdmin,
+                                              name: e.target.value,
+                                          })
+                                        : setNewAdminName(e.target.value)
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Email:</label>
+                            <input
+                                type="email"
+                                value={
+                                    editingAdmin
+                                        ? editingAdmin.email
+                                        : newAdminEmail
+                                }
+                                onChange={(e) =>
+                                    editingAdmin
+                                        ? setEditingAdmin({
+                                              ...editingAdmin,
+                                              email: e.target.value,
+                                          })
+                                        : setNewAdminEmail(e.target.value)
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Password:</label>
+                            <input
+                                type="password"
+                                value={
+                                    editingAdmin
+                                        ? editingAdmin.password
+                                        : newAdminPassword
+                                }
+                                onChange={(e) =>
+                                    editingAdmin
+                                        ? setEditingAdmin({
+                                              ...editingAdmin,
+                                              password: e.target.value,
+                                          })
+                                        : setNewAdminPassword(e.target.value)
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Is Super:</label>
+                            <Button
+                                type={newAdminIsSuper ? "primary" : "default"}
+                                onClick={() =>
+                                    setNewAdminIsSuper(!newAdminIsSuper)
+                                }
+                            >
+                                {newAdminIsSuper ? "Yes" : "No"}
+                            </Button>
+                        </div>
                     </Modal>
+
                     <input
                         type="text"
                         placeholder="Search by name, email or role"
@@ -178,15 +277,7 @@ function AdminTable() {
                                     )}
                                 </td>
                                 <td>
-                                    {editingAdmin?.id === admin.id ? (
-                                        <button
-                                            onClick={() =>
-                                                handleSaveAdmin(editingAdmin)
-                                            }
-                                        >
-                                            Save
-                                        </button>
-                                    ) : (
+                                    {editingAdmin?.id === admin.id ? null : (
                                         <>
                                             <button
                                                 onClick={() =>
