5a5778d1deefbd929558ab83e6fa221a4b76ddd9
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-05-04 09:37:27 +0300

    edit on footer

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -11,19 +11,9 @@ import {
 function Footer() {
   return (
     <footer>
-      <div className="parent_container">
-
-
+      {/* <div className="parent_container">
         <div className="footer-container1  boxReverse">
-          {/* <img
-            src="https://www.figma.com/file/8WOK9IiiJJFwH4mokwCJwH/eCommerce?node-id=2-193&t=53eWlZkaC4YKoDmI-4"
-            alt="logo"
-          /> */}
-
-          <img
-            src="http://placekitten.com/g/50/50"
-            alt="logo"
-          />
+          <img src="http://placekitten.com/g/50/50" alt="logo" />
           <p className="copyright">Copyright @ 2023</p>
 
           <div className="social-icons-container">
@@ -40,23 +30,35 @@ function Footer() {
               <FontAwesomeIcon icon={faWhatsapp} size="2x" />
             </a>
           </div>
+        </div> */}
 
-
-        </div>
+      <div className="parent_container">
         <div className="footer-container1">
           <h3>PRODUCTS</h3>
-          <NavLink to="/HomeMade Food " className="links">HomeMade Food </NavLink>
-          <NavLink to="/" className="links">Recycling </NavLink>
-          <NavLink to="/"  className="links">Hygienic </NavLink>
+          <NavLink to="/HomeMade Food " className="links">
+            HomeMade Food{" "}
+          </NavLink>
+          <NavLink to="/" className="links">
+            Recycling{" "}
+          </NavLink>
+          <NavLink to="/" className="links">
+            Hygienic{" "}
+          </NavLink>
         </div>
         <div className="footer-container1   boxReverse">
           <h3>COMPANY</h3>
-          <NavLink to="/About" className="links">About Us</NavLink>
-          <NavLink to="/Event" className="links">Events</NavLink>
-          <NavLink to="/Contact" className="links">Contact Us</NavLink>
+          <NavLink to="/About" className="links">
+            About Us
+          </NavLink>
+          <NavLink to="/Event" className="links">
+            Events
+          </NavLink>
+          <NavLink to="/Contact" className="links">
+            Contact Us
+          </NavLink>
         </div>
         <div className="footer-container1   _location__box">
-          <h3 className="location_"  >LOCATION</h3>
+          <h3 className="location_">LOCATION</h3>
           <p className="location_">Coming Soon to </p>
           <p className="location_">
             Phone: <a href="tel:7129406">7129406</a>
@@ -64,6 +66,26 @@ function Footer() {
           <p className="location_  Email">
             Email: <a href="mailto:shop@gmail.com">shop@gmail.com</a>
           </p>
+        </div>
+
+        <div className="footer-container1  boxReverseSocial">
+          <img src="http://placekitten.com/g/50/50" alt="logo" />
+          <p className="copyright">Copyright @ 2023</p>
+
+          <div className="social-icons-container">
+            <a href="https://www.facebook.com/">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://twitter.com/">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a href="https://whatsapp.com/">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
