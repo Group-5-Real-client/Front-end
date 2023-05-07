e6901025489591129937c76d27d0122937cf0bb4
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-05-07 19:20:29 +0300

    fixing routes

--- src/App.js
+++ src/App.js
@@ -11,45 +11,44 @@ import Products from "./Pages/Products/Product";
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
 import Signup from "./Pages/Signup";
-import  ProductPage from './Pages/Singleproduct/index'
+import ProductPage from "./Pages/Singleproduct/index";
 import FoodPage from "./Pages/FoodPage";
-import Recycling  from "./Pages/Recycling";
+import Recycling from "./Pages/Recycling";
 import Hygienic from "./Pages/Hygienic";
 
-
-
-
-
-
+const NotFound = () => <div>Page not found</div>;
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="Our-story" element={<AboutUs />} />
-        <Route path="Event" element={<Event />} />
-        <Route path="Contact" element={<ContactUs />} />
-        <Route path="Category" element={<Category />} />
-        <Route path="Product" element={<Products />} />
-        <Route path="OneProduct" element={<ProductPage />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="" element={<Home />} />
-        <Route path="Register" element={<Signup />} />
-        <Route path="FoodProduct" element={<FoodPage />} />
-        <Route path="Recycling" element={<Recycling />} />
-        <Route path="Hygienic" element={<Hygienic/>} />
+    const isLoggedIn = false;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                {!isLoggedIn ? (
+                    <>
+                        <Route path="Login" element={<Login />} />
+                        <Route path="Register" element={<Signup />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="Contact" element={<ContactUs />} />
 
-
-
-  
-        
-
-        
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+                        <Route path="OneProduct" element={<ProductPage />} />
+                        <Route path="" element={<Home />} />
+                        <Route path="FoodProduct" element={<FoodPage />} />
+                        <Route path="Recycling" element={<Recycling />} />
+                        <Route path="Hygienic" element={<Hygienic />} />
+                    </>
+                )}
+                <Route path="Our-story" element={<AboutUs />} />
+                <Route path="Event" element={<Event />} />
+                <Route path="Category" element={<Category />} />
+                <Route path="Product" element={<Products />} />
+                <Route path="*" exact={true} element={<NotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
