f71d5f39462bb5d19e6246958160011591d0c3fc
Aya Shahal <ayashahal055@gmail.com>
2023-05-07 21:56:20 +0300

    done

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
 import Signup from "./Pages/Signup";
 import  ProductPage from './Pages/Singleproduct/index'
-import FoodPage from "./Pages/FoodPage";
-import Recycling  from "./Pages/Recycling";
-import Hygienic from "./Pages/Hygienic";
+
 function App() {
   return (
     <BrowserRouter>
@@ -25,13 +23,11 @@ function App() {
         <Route path="Contact" element={<ContactUs />} />
         <Route path="Category" element={<Category />} />
         <Route path="Product" element={<Products />} />
-        <Route path="OneProduct" element={<ProductPage />} />
+        <Route path="/OneProduct/:productId" element={<ProductPage />} />
         <Route path="Login" element={<Login />} />
         <Route path="" element={<Home />} />
         <Route path="Register" element={<Signup />} />
-        <Route path="FoodProduct" element={<FoodPage />} />
-        <Route path="Recycling" element={<Recycling />} />
-        <Route path="Hygienic" element={<Hygienic/>} />
+        <Route path=":category?" element={<Products />}/>
       </Routes>
       <Footer />
     </BrowserRouter>
